78c4bfb31b7c3066b10e35829b6233b0b5be73df
Mihai Diaconu <mihai.diaconu05@e-uvt.ro>
2025-06-02 18:52:04 +0300

    Delete Reparatie.cpp

--- Reparatie.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Mihai on 6/2/2025.
-//
-
-#include "Reparatie.h"
-
-
-Reparatie::Reparatie(double pretPeOra, const vector<Piesa>& piese)
-    : pretPeOra(pretPeOra), piese(piese) {}
